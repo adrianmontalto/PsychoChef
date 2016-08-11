2951ee6937472cf0bbe1c4fdce33145485e188cc
Tennyson King <toptenny@hotmail.com>
2016-08-11 15:06:31 +1000

    Added: Fork pick up script. Collision layers. (Works with fork pick up setup).

---
+++ Assets/ForkPickup.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForkPickup : MonoBehaviour
+{
+    FixedJoint fixedJoint;
+
+
+	// Use this for initialization
+	void Start ()
+    {
+
+	}
+	
+
+	// Update is called once per frame
+	void Update ()
+    {
+
+	}
+
+
+    void OnTriggerEnter (Collider other)
+    {
+        fixedJoint = gameObject.AddComponent<FixedJoint>();
+
+        fixedJoint.breakForce = 1000;
+
+        fixedJoint.connectedBody = other.transform.root.GetComponent<Rigidbody>();
+    }
+
+    void OnJointBreak (float breakForce)
+    {
+        Destroy(fixedJoint);
+    }
+}
