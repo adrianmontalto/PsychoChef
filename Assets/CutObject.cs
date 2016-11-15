d017b165ce8f6b36e284e23a5c868c1e24cae623
AIE\adrian.montalto <adrianmontalto2@gmail.com>
2016-11-16 09:33:10 +1100

    merged

---
+++ Assets/CutObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnCollisionEnter (Collision col)
+    {
+        if (col.gameObject.tag == "Food")
+        {
+            if (col.gameObject.GetComponent<SlicedObject>().GetRecentlySliced() == false)
+            {
+                StartCoroutine(BLINDED_AM_ME.MeshCut.Cut(col.gameObject, transform.position, transform.up, col.gameObject.GetComponent<Renderer>().material));
+            }
+        }
+    }
+}
